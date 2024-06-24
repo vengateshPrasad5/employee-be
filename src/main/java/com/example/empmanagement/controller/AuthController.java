bbef08067d1849d1876b21ee265b8f8f252cfe2e
Vengatesh <venkuvenky2@gmail.com>
2024-06-24 19:14:35 +0530

    exception handled

--- src/main/java/com/example/empmanagement/controller/AuthController.java
+++ src/main/java/com/example/empmanagement/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.example.empmanagement.controller;
 import com.example.empmanagement.dto.JwtAuthResponse;
 import com.example.empmanagement.dto.LoginRequestDto;
 import com.example.empmanagement.dto.RegisterDto;
+import com.example.empmanagement.exception.ResourceNotFoundException;
 import com.example.empmanagement.service.AuthService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,16 +18,25 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("login")
-    public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginRequestDto requestDto) {
-        JwtAuthResponse jwtAuthResponse = authService.login(requestDto);
+    public ResponseEntity<Object> login(@RequestBody LoginRequestDto requestDto) {
+        try {
+            JwtAuthResponse jwtAuthResponse = authService.login(requestDto);
 
-        return ResponseEntity.ok(jwtAuthResponse);
+            return new ResponseEntity<>(jwtAuthResponse,HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+           throw new ResourceNotFoundException("User Not found");
+//           return new ResponseEntity<>("User Not found",HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         String response = authService.register(registerDto);
 
-        return ResponseEntity.ok(response);
+        try {
+            return ResponseEntity.ok(response);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
     }
 }
